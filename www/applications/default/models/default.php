2f5b8fce8779c8667060dc30a3ef9d204d95ba57
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-03-03 00:36:12 -0600

    cambios en tablas y base de datos

--- www/applications/default/models/default.php
+++ www/applications/default/models/default.php
@@ -17,17 +17,17 @@ class Default_Model extends ZP_Model {
 	}
 	
 	public function getCities() {
-		$data = $this->Db->query("select distinct estado, nestado from profeco order by estado asc");
+		$data = $this->Db->query("select distinct id_state, state from profeco2 order by id_state asc");
 		
 		foreach($data as $key=> $value) {
-			$data[$key]["nestado"] = utf8_decode($value["nestado"]);
+			$data[$key]["state"] = utf8_decode($value["state"]);
 		}
 		
 		return $data;
 	}
 	
 	public function getCategories() {
-		$data = $this->Db->query("select id, name from catalogo");
+		$data = $this->Db->query("select id, name from catalog");
 		
 		foreach($data as $key=> $value) {
 			$data[$key]["name"] = utf8_decode($value["name"]);
